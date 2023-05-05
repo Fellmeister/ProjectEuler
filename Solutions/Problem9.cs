0569dfbd177100c79538ee5de2959c355e2d436d
Fellmeister <fellmeister@hotmail.com>
2023-05-05 10:43:09 +1000

    Problem layout and thoughts...

---
+++ Solutions/Problem9.cs
@@ -0,0 +1,35 @@
+using Shouldly;
+
+namespace Test;
+
+public class Problem9
+{
+    // Problem 9
+    // Special Pythagorean triplet
+
+    // A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+    //          a² + b² = c²
+    
+    //          For example, 3² + 4² = 5²
+    //                      = 9 + 16 = 25 .
+    
+    // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    // Find the product abc.
+
+    // Thoughts/rules...
+    //      a < b < c
+    //      a + b + c = 1000
+    //      a² + b² = c²
+    // Answer: Find product of abc =  a * b * c 
+    
+    // TODO: Find all combos of  a + b + c = 1000
+    // TODO: Find combos of  a + b + c = 1000 where a² + b² = c²
+    // TODO: Find product of abc =  a * b * c
+    
+}
+
+public static class Problem9Solver
+{
+
+}
