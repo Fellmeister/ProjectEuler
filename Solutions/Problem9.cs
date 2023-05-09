254dd68f60f5b3b94ccc547539d1489d5f336191
Fellmeister <fellmeister@hotmail.com>
2023-05-09 16:23:34 +1000

    This does not work. I believe there is something fundamental going on here thatI'm not privvy to.

--- Solutions/Problem9.cs
+++ Solutions/Problem9.cs
@@ -26,10 +26,76 @@ public class Problem9
     // TODO: Find all combos of  a + b + c = 1000
     // TODO: Find combos of  a + b + c = 1000 where a² + b² = c²
     // TODO: Find product of abc =  a * b * c
-    
+
+    [Fact]
+    public void ShouldReturnCombinationsOfThreeNaturalNumbersThatEqual1000()
+    {
+        var result = Problem9Solver.GetThreeNumbersThatAddTo1000();
+
+        result.ShouldNotBeNull();
+        result.Count().ShouldNotBe(0);
+        foreach (var multiple in result)
+        {
+            multiple.Sum.ShouldBe(1000);
+        }
+    }
 }
 
 public static class Problem9Solver
 {
+    
+    public static List<Multiples> GetThreeNumbersThatAddTo1000()
+    {
+        List<int> numbers = new List<int>();
 
+        for (int i = 1; i < 700; i++)
+        {
+            numbers.Add(i);
+        }
+        
+        int target = 1000;
+        sum_up(numbers, target);
+        
+        
+        return new List<Multiples>();
+    }
+    
+    
+
+    private static void sum_up(List<int> numbers, int target)
+    {
+        sum_up_recursive(numbers, target, new List<int>());
+    }
+
+    private static void sum_up_recursive(List<int> numbers, int target, List<int> partial)
+    {
+        int s = 0;
+        foreach (int x in partial) s += x;
+
+        if (s == target && partial.Count() == 3)
+            Console.WriteLine("sum(" + string.Join(",", partial.ToArray()) + ")=" + target);
+
+        if (s >= target)
+            return;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            List<int> remaining = new List<int>();
+            int n = numbers[i];
+            for (int j = i + 1; j < numbers.Count; j++) remaining.Add(numbers[j]);
+
+            List<int> partial_rec = new List<int>(partial);
+            partial_rec.Add(n);
+            sum_up_recursive(remaining, target, partial_rec);
+        }
+    }
+}
+
+public class Multiples
+{
+    public int A;
+    public int B;
+    public int C;
+
+    public int Sum => A + B + C;
 }
